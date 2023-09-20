ee1fa76c6ec851c4ed04d77641ed1a4d63edb8b9
Sikha P <sikha.p@automationanywhere.com>
2023-09-20 12:51:34 +0530

    Integrated bot deployment with the website

--- JSO_FIN/PythonPackage/Libs/Twilio.py
+++ JSO_FIN/PythonPackage/Libs/Twilio.py
@@ -5,14 +5,20 @@ from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 
-account_sid = "AC6028e8d4d5cd65efe693b7de8885fe72"
-auth_token = "2934e22bfa5a2738295452fbb7506871"
+account_sid = "AC7cc150681032e3e8c4cf123ed4dddd36" #"AC6028e8d4d5cd65efe693b7de8885fe72"
+auth_token = "3096a51d8f650ac38927d3b1c212c54f" #"2934e22bfa5a2738295452fbb7506871"
 client = Client(account_sid, auth_token)
 
+call = client.calls.create(
+  twiml='<Response><Say voice="woman">Hey, New offers for you!Please check out your email for more details. Thank you</Say></Response>',
+  to="+918157897518",
+  from_="+14786062504"
+)
+"""
 call = client.calls.create(
   url="http://demo.twilio.com/docs/voice.xml",
   to="+917909140656",
   from_="+17079407687"
 )
-
-print(call.sid)
+"""
+#print(call.sid)
--- JSO_FIN/PythonPackage/api.py
+++ JSO_FIN/PythonPackage/api.py
@@ -1,3 +1,4 @@
+import requests
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity,get_jwt
 from .AppInit import app, api, fields, Resource, ns, request, client, User_Collection, db, render_template
 from .Models import login_user_model, user_model, customer_model
@@ -5,6 +6,8 @@ from .Libs.DataSimulation import generate_fake_customer, generate_OTP
 import json
 from bson.json_util import dumps
 from bson.objectid import ObjectId
+from datetime import datetime
+
 from bson.int64 import Int64
 #-------------------------
 
@@ -50,16 +53,23 @@ class OTPLogin(Resource):
     #@api.expect(login_user_model, validate=True)
     def post(self):
         data = request.get_json()
+        print("sasadasdasd")
         print(data)
         otp = data['otp']
         offerID = data['offerID']
         offer_collection = db["offers"]
         #offer_data = offer_collection.find_one({'_id': ObjectId(offerID)})
         offer_data = offer_collection.find_one({'_id': ObjectId(offerID), 'OTP': otp})
+        print("offer_data[customerDetails]")
+        customerObjID = offer_data['customerDetails']["_id"]["$oid"]
+        print(customerObjID)
         print(offer_data)
         if offer_data:
             additional_claims = {"roles": ["customer"]}
             access_token = create_access_token(identity=offerID, additional_claims=additional_claims)
+            print(data)
+            # Trigger the loan process at this point using deployment API
+            deployTriggerLoanProcessBot(offerID,customerObjID,"http://localhost:5000/jso/getCustomerDetailsv2")
             return {'status':'PASS', 'access_token': access_token}, 200
         else:
             return {'status':'FAIL', 'message': 'Invalid credentials'}, 200
@@ -178,7 +188,25 @@ class sendOTP(Resource):
             return {'status': 'Customer Fake Creation Failed'}, 401
 
 
+@ns.route('/ProceedWithOffer')
+class ProceedWithOffer(Resource):
+    #@api.expect(user_model, validate=True)
+    def post(self):
+        print("inside")
+        data = request.get_json()
+        print(data)
 
+        offer_collection = db["offers"]
+        offer_data = offer_collection.find_one({'_id': ObjectId(data['offerID'])})
+        print(offer_data)
+        otp = offer_data['OTP']
+        print("before deploy")
+        deploySendOTPBot("+918157897518", otp, "psdaa48@gmail.com")
+
+        print(data)
+
+        #
+        return {'status': 'Thanks for choosing the offer. You will get a notification soon'}, 200
 @ns.route('/getCustomers')
 class getCustomers(Resource):
     #@api.expect(customer_model, validate=True)
@@ -207,6 +235,25 @@ class getCustomerDetails(Resource):
            # list_cur = list(customer_data)
             res= dumps(customer_data, indent=2)
             return {'CustomerDetails': res},200
+        else:
+            return {'status': 'Customer Details Fetch Failed'}, 401
+
+
+@ns.route('/getCustomerDetailsv2')
+class getCustomerDetailsv2(Resource):
+    #@api.expect(customer_model, validate=True)
+    def post(self):
+        data = request.get_json()
+        custID = data['id']
+        print(custID)
+        customer_collection = db["customers"]
+        customer_data = customer_collection.find_one({'_id': ObjectId(custID)})
+        if customer_data is not None:
+           # list_cur = list(customer_data)
+            res = dumps(customer_data, indent=4)
+            print("res['DOB']")
+            print(customer_data['DOB'])
+            return { 'CustomerDetails' : res },200
         else:
             return {'status': 'Customer Details Fetch Failed'}, 401
 
@@ -312,3 +359,155 @@ class postOfferDetails(Resource):
             return {'status': "updated"},200 #mongo collection updated
         else:
             return {'status': 'Offer Details update Failed'}, 401
+
+#RPA Related functions
+
+
+def read_config_file(file_path):
+    try:
+        with open(file_path, "r") as config_file:
+            config_data = json.load(config_file)
+        return config_data
+    except FileNotFoundError:
+        print(f"Config file '{file_path}' not found.")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON in '{file_path}': {e}")
+        return None
+
+
+config_data = read_config_file(
+    r"C:\Users\Sikha.P\OneDrive - Automation Anywhere Software Private Limited\AA_SIKHA\AA_SIKHA\PROJECTS\JSO\BackEnd\Git\JSO\JSO_FIN\PythonPackage\config.json")
+
+
+def Authentication():
+    # function body
+    print(config_data)
+    print(config_data['ControlRoomUrl'])
+    api_url = config_data['ControlRoomUrl']+"/v1/authentication"
+    input_data = {
+        "username": config_data['username'],
+        "password": config_data['password']
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "accept": "application/json"
+    }
+
+
+    # Send a POST request to the API with input data and headers
+    response = requests.post(api_url, json=input_data, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse and use the response data (assuming it's in JSON format)
+        data = response.json()
+        print("API Response:")
+        print(data['token'])
+        return data['token']
+    else:
+        #print(f"API request failed with status code {response.status_code}")
+        return "API request failed with status code "
+    #print(f"API request failed: {e}")
+def deploySendOTPBot(mobileNumber,otp,toAddress):
+    # function body
+    print(config_data['ControlRoomUrl'])
+    api_url = config_data['ControlRoomUrl'] + "/v4/automations/deploy"
+    input_data = {
+        "botId": config_data['SendOTPBot'],
+        "botInput": {
+            "mobileNumber": {
+                "type": "STRING",
+                "string": mobileNumber
+            },
+            "logFilePath": {
+                "type": "STRING",
+                "string": config_data['logFilePath']
+            },
+            "otp": {
+                "type": "STRING",
+                "string": otp
+            },
+            "ToAddress": {
+                "type": "STRING",
+                "string": toAddress
+            }
+        },
+        "automationPriority": "PRIORITY_MEDIUM",
+        "unattendedRequest": {
+            "runAsUserIds":config_data['runAsUserIds'],
+            "poolIds":config_data['poolIds'],
+            "numOfRunAsUsersToUse": 0,
+            "deviceUsageType": config_data['deviceUsageType']
+        }
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "accept": "*/*",
+        "X-Authorization": Authentication()
+    }
+
+    # Send a POST request to the API with input data and headers
+    response = requests.post(api_url, json=input_data, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse and use the response data (assuming it's in JSON format)
+        data = response.json()
+        print("API Response:")
+        print(data)
+        return data
+    else:
+        print(f"API request failed with status code {response.status_code}")
+        return "API request failed with status code "
+    # print(f"API request failed: {e}")
+
+def deployTriggerLoanProcessBot(offerID,customerObjID,getCustomerDetailsAPIUrl):
+    # function body
+    print(config_data['ControlRoomUrl'])
+    api_url = config_data['ControlRoomUrl'] + "/v4/automations/deploy"
+    input_data = {
+        "botId": config_data['TriggerLoanProcessBot'],
+        "botInput": {
+            "offerID": {
+                "type": "STRING",
+                "string": offerID
+            },
+            "customerObjID": {
+                "type": "STRING",
+                "string": customerObjID
+            },
+            "getCustomerDetailsAPIUrl": {
+                "type": "STRING",
+                "string": getCustomerDetailsAPIUrl
+            }
+        },
+        "automationPriority": "PRIORITY_MEDIUM",
+        "unattendedRequest": {
+            "runAsUserIds":config_data['runAsUserIds'],
+            "poolIds":config_data['poolIds'],
+            "numOfRunAsUsersToUse": 0,
+            "deviceUsageType": config_data['deviceUsageType']
+        }
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "accept": "*/*",
+        "X-Authorization": Authentication()
+    }
+
+    # Send a POST request to the API with input data and headers
+    response = requests.post(api_url, json=input_data, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse and use the response data (assuming it's in JSON format)
+        data = response.json()
+        print("API Response:")
+        print(data)
+        return data
+    else:
+        print(f"API request failed with status code {response.status_code}")
+        return "API request failed with status code "
+    # print(f"API request failed: {e}")
+
